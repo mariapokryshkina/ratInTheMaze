3e283e7815e1429a995b1e96186d718b86f9fda2
Maria <bc-macair023@BCmacair023sAir.wlan>
2022-02-13 23:50:05 +0200

    first commit

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import Maze from './Maze';
+
+const element = <Maze> </Maze>
+
+ReactDOM.render(element, document.getElementById('root')
+);
+
+
